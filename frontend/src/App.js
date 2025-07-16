977349b340d5e7c1a1bfcee2dc7e7f82c9128d0f
AkshatK6971 <reachakshat6@gmail.com>
2025-07-16 13:41:52 +0530

    backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://tweet-intelligence-engine-backend.onrender.com";
 
 function App() {
   const [generatedTweet, setGeneratedTweet] = useState("");
